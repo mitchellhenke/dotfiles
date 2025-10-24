789204170625e3ab6a442acfab6afe05c41d3d97
Mitchell Henke <mitchell@mitchellhenke.com>
2025-10-24 11:22:07 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,14 @@ set EDITOR nvim
 set FZF_DEFAULT_COMMAND 'ag -g ""'
 set FZF_CTRL_T_COMMAND 'ag -g ""'
 set -x ERL_AFLAGS '-kernel shell_history enabled'
+set -x HOMEBREW_NO_AUTO_UPDATE '1'
+set -x HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK '1'
+# Something changed in Fish 4.1
+set -x fish_term24bit 0
 
 set fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 set fish_user_paths "/Applications/Postgres.app/Contents/Versions/11/bin" $fish_user_paths
+
 
 # gem_home
 # source "/usr/local/share/gem_home/gem_home.sh"
@@ -17,4 +22,3 @@ source /usr/local/share/chruby/chruby.fish
 # kiex
 test -s "/Users/mitchellhenke/.kiex/scripts/kiex.fish"; and source "/Users/mitchellhenke/.kiex/scripts/kiex.fish"
 set fish_user_paths "/usr/local/opt/python/libexec/bin" $fish_user_paths
-
